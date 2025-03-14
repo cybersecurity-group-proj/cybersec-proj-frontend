9022d1559837d42b1ea9fa99788adfb179419e0e
Ahmed-aa1907019 <106153524+Ahmed-aa1907019@users.noreply.github.com>
2025-03-14 08:40:36 +0300

    Revert "Create ages"
    This reverts commit 07d8967c9eb7497e30b13e22cb50f8bf5de53bab.

--- cybersec-proj-frontend/src/app/(auth)/signup/page.jsx
+++
@@ -1,9 +0,0 @@
-import SignupForm from '@/components/auth/SignupForm'
-
-export default function SignupPage() {
-  return (
-    <div className="container mx-auto py-16">
-      <SignupForm />
-    </div>
-  )
-}
